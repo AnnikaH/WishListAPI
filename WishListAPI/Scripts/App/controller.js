8efd6799808310e11a5a97e5c09c4a1253a2085b
AnnikaH <annika_limfe@hotmail.com>
2015-11-19 20:40:45 +0100

    Added angular controller and index.html to test

---
+++ WishListAPI/Scripts/App/controller.js
@@ -0,0 +1,14 @@
+﻿var App = angular.module("App", []);
+
+App.controller("faqController", function ($scope, $http, $location, $anchorScroll) {
+
+    var urlUser = '/api/User';
+
+    $http.get(urlUser + "/" + 1).
+        success(function (user) {
+            $scope.loading = true;
+        }).
+        error(function (data, status) {
+            
+        });
+});
