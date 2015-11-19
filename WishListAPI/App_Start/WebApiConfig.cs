7d2c34470ea2f1e0fa01436508fb259671b7d824
AnnikaH <annika_limfe@hotmail.com>
2015-11-19 17:36:25 +0100

    Update WebApiConfig

--- WishListAPI/App_Start/WebApiConfig.cs
+++ WishListAPI/App_Start/WebApiConfig.cs
@@ -13,10 +13,15 @@ namespace WishListAPI
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                //routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiWithAction",
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
